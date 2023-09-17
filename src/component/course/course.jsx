8f13d263d558121f0a6d8c0e07f39d862a618e46
DelwarHosssain <mddelwarhossainnobin12@gmail.com>
2023-09-17 13:34:45 +0600

    deshbord-push-total-price

--- src/component/course/course.jsx
+++ src/component/course/course.jsx
@@ -1,19 +1,43 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import { useEffect, useState } from "react";
 import Deshbord from "../deshbord/deshbord";
 
 const Course = () => {
 
-    const[course, setcourse] =useState([])
+    const[course, setCourse] =useState([]);
+    const[deshbordData, setDeshbordData] =useState([]);
+    const [totalRemaining, setTotalRemaining]=useState(0)
+    const [TotalTime, setTotalTime]=useState(0)
+
     useEffect(() =>{
         fetch('Course.json')
         .then(res => res.json())
-        .then(data => setcourse(data))
+        .then(data => setCourse(data))
     },[])
 
-    const hanglarDeshbord=()=>{
-        console.log('iam here')
+    const hanglarDeshbord=(course)=>{
+        // eslint-disable-next-line no-unused-vars
+        const isExist = deshbordData.find((itam) => itam.id == course.id)
+        
+        let count = parseInt(course.time);
+
+        if(isExist){
+           return alert("all ready book");
+            }
+            else{
+                deshbordData.forEach((item) =>{
+                    count+= parseInt(item.time);;
+                })
+                console.log(count);
+                const totalRemaining =20-count;
+                setTotalTime(count);
+                setTotalRemaining(totalRemaining);
+                setDeshbordData([...deshbordData,course]);
+        }
+        
     }
+    console.log(deshbordData); 
     
     return (
         <div className="mx-14  md:flex md:container md:mx-auto xl:container xl:mx-auto">
@@ -30,19 +54,20 @@ const Course = () => {
                         <h3 className="text-2xl">{course.bookmark_icon}</h3>
                         <h2 className="text-base  font-medium text-[#737272]">Credit: {course.time}</h2>
                     </div>
-                    <div onClick={() => hanglarDeshbord()} className="w-full bg-[#2f80ed] text-center rounded-lg p-2 ">
+                    <div onClick={() => hanglarDeshbord(course)} className="w-full bg-[#2f80ed] text-center rounded-lg p-2 ">
                         <button className="text-lg font-medium text-white">Select</button>
                    </div>
                 </div>
                     ))
                 }
             </div>
-             <Deshbord>
-                <div className="md:w-1/4">
-
-                </div>
+             
+             <div className="md:w-1/4">
+                <div className="bg-white p-5 shadow-lg shadow-indigo-500/50 rounded-lg space-y-4 gap-5 ml-12 w-96">
+             <Deshbord deshbordData={deshbordData} totalRemaining={totalRemaining} TotalTime={TotalTime} >
              </Deshbord>
-            
+             </div>
+             </div>
         </div>
     );
 };
--- src/component/deshbord/deshbord.jsx
+++ src/component/deshbord/deshbord.jsx
@@ -1,8 +1,27 @@
+/* eslint-disable react/prop-types */
 
-const Deshbord = () => {
+// eslint-disable-next-line react/prop-types
+const Deshbord = ({deshbordData,totalRemaining,TotalTime}) => {
+    console.log(deshbordData);
+
     return (
-        <div className="">
-            <h1>Deshbord</h1>
+        <div className=" space-y-4">
+            <h1 className="text-lg font-bold text-[#2F80ED] ">Credit Hour Remaining: {totalRemaining} hr</h1>
+            <hr />
+            
+
+            {
+                // eslint-disable-next-line react/prop-types
+                deshbordData.map((Course) =>(
+                    <li key={Course.id} type="1">{Course.title}</li>
+                    
+                    
+                ))
+            }
+            <hr />
+            <h1>Total Credit Hour : {TotalTime}</h1>
+            <hr />
+            <h1>Total Price : 48000 USD</h1>
         </div>
     )
 };
